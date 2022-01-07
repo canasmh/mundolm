8dfefeb729b40052c22f575d12ce81aa468bbd4d
mannylinesofcode <canasmh@yahoo.com>
2022-01-06 23:33:42 -0500

    Add the pages used in nav

--- app.js
+++ app.js
@@ -11,6 +11,23 @@ app.get("/", function(req, res) {
   res.render('index');
 });
 
+app.get("/about", function(req, res) {
+  res.render('about');
+});
+
+app.get("/make-payment", function(req, res) {
+  res.render('make-payment');
+});
+
+app.get("/get-quote", function(req, res) {
+  res.render('get-quote');
+});
+
+app.get("/contact", function(req, res) {
+  res.render('contact');
+});
+
+
 app.listen(3000, function() {
   console.log("Server running on port 3000");
 });
