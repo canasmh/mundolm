b342b44c95b87b8cd404e6e95f04407ce95bf199
mannylinesofcode <canasmh@yahoo.com>
2022-01-11 16:58:55 -0500

    Added functionality to the our-team button

--- public/js/myscript.js
+++ public/js/myscript.js
@@ -1,30 +1,41 @@
+// Check to see if on homepage
+let onHomePage = true;
 
-$(document).ready(function() {
-  $('div.welcome-section').animate({left: '0', opacity: 1}, 750)
+try {
+  var textWrapper = document.querySelector('.motto');
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='motto'>$&</span>");
+}
+catch (TypeError) {
+  onHomePage = false;
+}
 
+if (onHomePage) {
+  $(document).ready(function() {
+    $('div.welcome-section').animate({left: '0', opacity: 1}, 750)
+
+  });
+
+  setTimeout(function() {
+    $('.welcome-banner').animate({opacity: 1, top: '0'}, 750);
+  }, 650);
+
+  setTimeout(function() {
+    anime.timeline({loop: false})
+      .add({
+        targets: '.motto',
+        scale: [6,1],
+        opacity: [0,1],
+        translateZ: 0,
+        easing: "easeOutExpo",
+        duration: 800,
+        delay: (el, i) => 65*i
+      });
+  }, 1500);
+}
+
+
+
+$('.team-btn-div').click(function() {
+  let element = document.getElementsByClassName("team-page")[0];
+  element.scrollIntoView({behavior: "smooth"});
 });
-
-setTimeout(function() {
-  $('.welcome-banner').animate({opacity: 1, top: '0'}, 750);
-}, 650);
-
-var textWrapper = document.querySelector('.motto');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='motto'>$&</span>");
-
-
-setTimeout(function() {
-  anime.timeline({loop: false})
-    .add({
-      targets: '.motto',
-      scale: [6,1],
-      opacity: [0,1],
-      translateZ: 0,
-      easing: "easeOutExpo",
-      duration: 800,
-      delay: (el, i) => 65*i
-    });
-}, 1500)
-
-// setTimeout(function() {
-//   $('nav').animate({opacity: 1}, 1200)
-// }, 2500);
