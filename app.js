b0122b95e91c950af7df80253a6e6dd00de6d0ad
mannylinesofcode <canasmh@yahoo.com>
2022-02-04 17:43:45 -0500

    Remove if statements in the welcome section from index.js and add them to the app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const ejs = require("ejs")
 const app = express();
 
 var lang = "en"
-
+// Header
 var titleEn = "Welcome | MLM"
 var titleEs = "Bienvenidos | MLM"
 var title = titleEn
@@ -14,12 +14,24 @@ var navEn = ["About", "Insurance", "Get a Quote", "Make a Payment", "Contact"]
 var navEs = ["Acerca de", "Seguros", "Conseguir Cotización", "Realizar un pago", "Contacto"]
 var nav = navEn
 
+//Index
+var welcomeEn = ["Welcome", "...We serve with joy! 😊", "En español", "Get Quote"]
+var welcomeEs = ["Bienvenidos", "...Servimos con alegría 😊", "In English", "Conseguir Cotización"]
+var welcome = ""
+
+
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.render('index', {lang: lang, title: title, nav: nav});
+  if (lang == "en") {
+    welcome = welcomeEn
+  } else {
+    welcome = welcomeEs
+  }
+  res.render('index', {lang: lang, title: title, nav: nav, welcome: welcome});
 });
 
 app.get("/about", function(req, res) {
