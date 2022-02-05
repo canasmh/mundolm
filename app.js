f4b89f0f5c720136ac4d55bbc849d9113abc8d9c
mannylinesofcode <canasmh@yahoo.com>
2022-02-04 20:06:25 -0500

    Remove if statements in the about.ejs and add them to the app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ class Service {
   }
 }
 
-class Team Member {
+class TeamMember {
   constructor(name, description, img) {
     this.name = name
     this.description = description
@@ -108,6 +108,63 @@ var ourTeamEn = "Our Team"
 var ourTeamEs = "Nuestro Equipo"
 var ourTeam = ourTeamEn
 
+var teamMemberOneEn = new TeamMember("Victor and Yaneth", "Victor and Yaneth had \
+been living in Greer for several years and saw the need for the Hispanic \
+community to have a place where the same language was spoken to them and where \
+their needs were fully met. Motivated by this great need, on November 27, Mundo \
+Latino Multiservicios opened its doors to that beautiful and brave Hispanic \
+community and to anyone who needed our services.", "images/team-img-test.png")
+var teamMemberTwoEn = new TeamMember("Nohemí Riaño", "Nohemí is a young and \
+charismatic woman, originally from Mexico, with more than five years of \
+experience and knowledge of our products. This makes her have an excellent \
+attention to our clients and an agile development in her service.",
+"images/team-img-test.png")
+var teamMemberThreeEn = new TeamMember("Andres Felipe Gonzalez", "A young \
+Colombian, who has accompanied us on this walk for more than four years. Andres \
+has a great knowledge, performance and development in systems, and can be trusted \
+to respond with great mastery and speed in the service of our excellent products.",
+"images/team-img-test.png")
+var teamMemberFourEn = new TeamMember("Norma Cinto", "A tender and simple \
+Guatemalan woman with more than three years she has delivered an excellent \
+service, of providing ours in a simple, honest and loving way.",
+"images/team-img-test.png")
+var teamMemberFiveEn = new TeamMember("Yuridia Hernandez", "Yuri as affectionate \
+as can be, offers us her excellent service from Mexico. She accompanied us in \
+person for more than four years and now offers us her great service virtually. \
+Yuri is agile, attentive and with a great commitment not only to our clients, \
+but also to Mundo Latino Multiservicios.", "images/team-img-test.png")
+var teamMemberOneEs = new TeamMember("Victor and Yaneth", "Victor y Yaneth, \
+llevaban varios anos viviendo en Greer y veían la necesidad de que tenia la \
+comunidad hispana de tener un lugar donde, se les hablara el mismo idioma y \
+donde sus necesidades fueran cubiertas en su totalidad. Motivados por esa gran \
+necesidad, que el día 27 de noviembre, Mundo Latino Multiservicios abrió sus \
+puertas a esa hermosa y valiente comunidad Hispana y a todo aquel, que necesitara \
+de nuestros servicios.", "images/team-img-test.png")
+var teamMemberTwoEs = new TeamMember("Nohemí Riaño", "Nohemí es una joven y \
+carismática mujer, oriunda de México, con mas de cinco anos de experiencia y \
+conocimiento de nuestros productos. Esto hace que tenga una excelente atención a \
+nuestro cliente y un ágil desenvolvimiento en sus servicio.",
+"images/team-img-test.png")
+var teamMemberThreeEs = new TeamMember("Andres Felipe Gonzalez", "Joven \
+Colombiano, que nos acompaña en este caminar por mas de cuatro años. Andres \
+tiene un gran conocimiento, desempeño y desenvolvimiento en sistemas, punto a \
+favor, para agilizar y responder con gran maestría en el servicio de nuestros \
+excelentes productos.", "images/team-img-test.png")
+var teamMemberFourEs = new TeamMember("Norma Cinto", "Una tierna y sencilla mujer \
+Guatemalteca con mas de tres años acompañados a entregar su excelente servicio, \
+a nuestros de una manera simple, honesta y amorosa.", "images/team-img-test.png")
+var teamMemberFiveEs = new TeamMember("Yuridia Hernandez", "Yuri como \
+cariñosamente, le dimos nosotros y nuestros clientes, nos ofrece su excelente \
+servicio desde México. Nos acompaño presencialmente por mas de cuatro años y \
+ahora nos ofrece su gran servicio de manera virtual. Yuri es ágil atenta y con \
+un gran compromiso no solo con nuestros clientes , sino con Mundo Latino \
+Multiservicios.", "images/team-img-test.png")
+var teamMembersEn = [teamMemberOneEn, teamMemberTwoEn, teamMemberThreeEn,
+  teamMemberFourEn, teamMemberFiveEn]
+var teamMembersEs = [teamMemberOneEs, teamMemberTwoEs, teamMemberThreeEs,
+  teamMemberFourEs, teamMemberFiveEs]
+var teamMembers = teamMembersEn
+
 // FOOTER
 var footerEn = ["Contact Us", "Mon-Sat", "Sun"]
 var footerEs = ["Contáctanos", "Lun-Sáb", "Dom"]
@@ -140,7 +197,20 @@ app.get("/", function(req, res) {
 });
 
 app.get("/about", function(req, res) {
-  res.render('about', {lang: lang, title: title, nav: nav, footer: footer});
+  if (lang=="en") {
+    ourTeam = ourTeamEn
+    teamMembers = teamMembersEn
+  } else {
+    ourTeam = ourTeamEs
+    teamMembers = teamMembersEs
+  }
+  res.render('about', {
+    lang: lang,
+    title: title,
+    nav: nav,
+    ourTeam: ourTeam,
+    teamMembers: teamMembers,
+    footer: footer});
 });
 
 app.get("/make-payment", function(req, res) {
