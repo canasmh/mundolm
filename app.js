efa33fe0e3e83215c3305cd4c026ae9e8183a49e
Manuel Canas <canasmh@yahoo.com>
2022-04-01 11:56:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const ejs = require("ejs");
 const nodemailer = require("nodemailer");
 const path = require('path');
 const fs = require('fs');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 
