d08b442a6b9b0eace43ba0146e6151917bdd2fc3
canasmh <canasmh@yahoo.com>
2022-04-08 15:09:43 -0400

    Write functionality for animations

---
+++ js/animate.js
@@ -0,0 +1,34 @@
+try {
+  var textWrapper = document.querySelector('.motto');
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='motto'>$&</span>");
+}
+catch (TypeError) {
+  onHomePage = false;
+}
+
+$(document).ready(function() {
+  $('div.welcome-section').animate({left: '0', opacity: 1}, 800)
+});
+
+setTimeout(function() {
+  $('.welcome-banner').animate({opacity: 1, top: '0'}, 700);
+  $('.scroll-down-div').animate({opacity: 1, top: '0'}, 700)
+}, 900);
+
+setTimeout(function() {
+  anime.timeline({loop: false})
+    .add({
+      targets: '.motto',
+      scale: [6,1],
+      opacity: [0,1],
+      translateZ: 0,
+      easing: "easeOutExpo",
+      duration: 800,
+      delay: (el, i) => 65*i
+    });
+}, 1900);
+
+$('.team-btn-div').click(function() {
+  let teamPage = document.getElementsByClassName("team-page")[0];
+  teamPage.scrollIntoView({behavior: "smooth"});
+});
